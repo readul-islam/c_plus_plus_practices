9b495ff52177ab9559fa004007e4e2ab3ba8da03
readul-islam <developer.readul@gmail.com>
2023-09-04 23:00:51 +0600

    update hm work

---
+++ problems/hm4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   for (int i = 0; i < 5; i++) {
+    cout << i << endl;
+      for (int j = i; j <= 5; j++) {
+      
+      cout << j << " " ;
+      }
+      cout << endl;
+   }
+}
