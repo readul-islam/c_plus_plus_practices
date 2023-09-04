2c8bbfcb954843823c1018bb9716b1c25928f944
readul-islam <developer.readul@gmail.com>
2023-09-04 22:16:47 +0600

    prime number check

---
+++ problems/primeNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n;
+   cout << "Enter your Number: ";
+   cin >> n;
+   for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+         cout << n << " isn't Prime Number" << endl;
+         return 0;
+      }
+   }
+
+   cout << n << " is Prime Number";
+}
