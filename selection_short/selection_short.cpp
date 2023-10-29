f2e02da1910c6d31501c6ea0b5d664506cf655e4
readul-islam <developer.readul@gmail.com>
2023-10-29 13:37:01 +0600

    complete selection sort

--- selection_short/selection_short.cpp
+++ selection_short/selection_short.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 using namespace std;
 
+void selection_sort(int *arr, int size) {
 
-void selection_sort(int arr[], int size){
+   for (int i = 0; i < size - 1; i++) {
 
-    for(int i =0; i< size-1; i++){
-        
-        int minIndex = i;
-      for(int j=i+1; j < size-1 ; j++){
-            if(arr[minIndex] < arr[j]){
-                minIndex = j;
-                cout<< arr[minIndex] << " " << minIndex<< endl;
-            }
+      int minIndex = i;
+      for (int j = i + 1; j <= size - 1; j++) {
+         if (arr[j] < arr[minIndex]) 
+            minIndex = j;
+         
       }
-    }
-
+      swap(arr[minIndex], arr[i]);
+   }
 }
 
-int main(){
+int main() {
 
-    int arr[6] = {6,5,2,7,1,3};
-    int size = 6;
-    selection_sort(arr, size);
-    return 0;
+   int arr[6] = {6, 5, 2, 7, 1, 3};
+   int size = 6;
+
+   for (int i = 0; i < size; i++) {
+      cout << arr[i] << " ";
+   }
+    cout << endl;
+   selection_sort(arr, size);
+   for (int i = 0; i < size; i++) {
+      cout << arr[i] << " ";
+   }
+   return 0;
 }
